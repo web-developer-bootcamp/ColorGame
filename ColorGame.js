b1711c24fbbfcf4188e2fc6ab37a2087f218794a
Yintong <zhouyintong96@gmail.com>
2019-08-08 17:16:05 +0200

    add check color

--- ColorGame.js
+++ ColorGame.js
@@ -9,8 +9,20 @@ var colors = [
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
 
+colorDisplay.textContent = pickedColor;
 
 for(var i=0; i<squares.length; i++){
     squares[i].style.background = colors[i];
+
+    squares[i].addEventListener("click", function(){
+        var clickedColor = this.style.background;
+
+        if(clickedColor === pickedColor){
+            alert('Correct')
+        }else{
+            alert('Wrong')
+        }
+    });
 }
